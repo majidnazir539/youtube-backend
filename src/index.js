24b3a92fe0644e323cc14a65e976d4a3567d1db2
Majid Nazir <majidnazir539@gmail.com>
2024-02-05 11:41:21 +0530

    added utils files

--- src/index.js
+++ src/index.js
@@ -5,7 +5,17 @@ import connectDB from "./db/index.js";
 
 dotenv.config({path: './env'})
 
+const port = process.env.PORT || 8000
+
 connectDB()
+.then(()=>{
+    app.listen(port, () =>{
+        console.log(`Sever is running at port: ${port}`);
+    })
+})
+.catch((err)=>{
+    console.log('MongoDb connection failed:', err);
+})
 
 
 
